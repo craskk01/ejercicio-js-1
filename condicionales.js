ee8bc341e8b269304328368e6977f212b5c49164
Craskk01 <luispinto0166@gmail.com>
2023-08-30 21:29:05 -0500

    ejercicio js 1

---
+++ condicionales.js
@@ -0,0 +1,9 @@
+const nombre = "Gredd0166";
+let edad = 23;
+let nota = 7;
+
+if(nombre === "Greed" && nota >= 6) {
+    console.log("Hola" + nombre ,"su nota es = " + nota);
+} else {
+    console.log("El nombre no existe")
+}
